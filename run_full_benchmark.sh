616cc83d1d8dad6ff29e872efa8d192dd9cbe895
Matheus Neves <matsmoraes19@gmail.com>
2025-12-04 14:05:12 +0000

    full_bechmark

---
+++ run_full_benchmark.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SIZES=(250 500 750 1000)
+HEADER_FILE="include/main.h"
+TEMP_RESULTS="all_hv_results.txt"
+
+for size in "${SIZES[@]}"
+do
+
+    sed -i "s/^#define NUM_ITENS.*/#define NUM_ITENS $size/" $HEADER_FILE
+    
+    
+    ./compile.sh > /dev/null 2>&1
+    
+    
+    rm -f $TEMP_RESULTS
+    
+    
+    for i in {1..30}
+    do
+        
+        ./output/main > /dev/null 
+        
+        
+        python calculate_hv.py
+    done
+    
+    
+    mv $TEMP_RESULTS "results_${size}_items.txt"
+done
